9823379242b47a10ca284080567d98ea024e93d1
olcereray2-ship-it <olcereray2@gmail.com>
2026-09-25 07:17:27 +0300

    Harden telemetry_local.gd behavior

--- scripts/telemetry_local.gd
+++ scripts/telemetry_local.gd
@@ -1,4 +1,11 @@
 class_name LocalTelemetry
+
 static func event(log:Array,name:String,data:Dictionary={}):
- log.append({"event":name,"data":data,"ts":Time.get_unix_time_from_system()})
+ var event_name=name.strip_edges()
+ if event_name.is_empty():return
+ log.append({
+  "event":event_name,
+  "data":data.duplicate(true),
+  "ts":Time.get_unix_time_from_system()
+ })
  while log.size()>200:log.pop_front()
